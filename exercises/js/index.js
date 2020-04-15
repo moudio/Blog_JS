b70400d6e31ef6346df25e78cde49957a1a833b0
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-04-15 13:15:02 +0000

    Connecting the front end with the api

--- exercises/js/index.js
+++ exercises/js/index.js
@@ -1,15 +1,38 @@
-
-
-const API_URL = "http://localhost:3000/api/posts";
-const API_BASE_URL = "http://localhost:3000/";
+const API_URL = 'http://localhost:3000/api/posts';
+const API_BASE_URL = 'http://localhost:3000/';
 
 window.onload = () => {
-    getPosts();
-}
+  getPosts();
+};
 
 const getPosts = () => {
-}
+  fetch(API_URL, {
+    method: 'GET',
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      buildPosts(data);
+    });
+};
 
 const buildPosts = (blogPosts) => {
+  let blogPostContent = '';
+  for (blogPost of blogPosts) {
+    const postDate = new Date(parseInt(blogPost.added_date)).toDateString();
+    const postImage = `${API_BASE_URL}${blogPost.post_image}`;
+    blogPostContent += `<div class="post">
 
-}
+                <div class="post-image" style="background-image:url(${postImage})"> </div>
+                <div class="post-content">
+                    <div class="post-date">${postDate}</div>
+                    <div class="post-title">
+                        <h4>${blogPost.title}</div>
+                    <div class="post-text">${blogPost.content}</div>
+                </div>
+            </div>
+ `;
+  }
+  document.querySelector('.blog-posts').innerHTML = blogPostContent;
+};
